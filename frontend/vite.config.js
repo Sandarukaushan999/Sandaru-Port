3edd0b02e362263abe22e64d03c157a9269dc251
Sandarukaushan999 <162014836+Sandarukaushan999@users.noreply.github.com>
2025-06-27 03:08:49 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: '/SANDARU-PORT/' // <-- This must match your repo name exactly!
+  base: '/Sandaru-Port/' // <-- This must match your repo name exactly!
 })
